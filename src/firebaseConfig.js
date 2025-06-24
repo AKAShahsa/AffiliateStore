9d06873a33e7585348f74175a4ebaea9a743610d
Syed Taha Hussain Shah <91339674+AKAShahsa@users.noreply.github.com>
2025-06-25 01:35:50 +0500

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuwefOR0oGyhmIYcKX3WkBXaTn5DoxHTE",
-  authDomain: "affiliate-store-5792e.firebaseapp.com",
-  databaseURL: "https://affiliate-store-5792e-default-rtdb.firebaseio.com",
-  projectId: "affiliate-store-5792e",
-  storageBucket: "affiliate-store-5792e.appspot.com",
-  messagingSenderId: "922571468296",
-  appId: "1:922571468296:web:52104b59598e29c87f2bbd",
-  measurementId: "G-3TJL5JDKMH"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
